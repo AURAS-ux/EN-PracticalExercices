fc85c62b634d5904d6840fe6a941d21949aa675a
pauras <pauras@ENDAUTOHVJGP4WR>
2025-12-20 16:14:59 +0200

    fix: Update UpcomingFlightsDto to initialize Date property to prevent null reference issues

--- Airport Management Tool/AMT.Api/db-tests.sql
+++ Airport Management Tool/AMT.Api/db-tests.sql
@@ -1,9 +1,6 @@
 DELETE FROM [AirportManagement].[dbo].[Flight]
 WHERE Id = 1;
 
-INSERT INTO Gate ([AirportId], [Code])
-VALUES (1, 'A1'), (1, 'A2'), (2, 'B1'), (2, 'B2');
-
 
 SELECT TOP (1000) [Id]
       ,[AirlineId]
--- Airport Management Tool/AMT.Application/Dtos/UpcomingFlightsDto.cs
+++ Airport Management Tool/AMT.Application/Dtos/UpcomingFlightsDto.cs
@@ -2,6 +2,6 @@ namespace AMT.Application.Dtos;
 
 public record class UpcomingFlightsDto
 {
-    public string Date { get; set; }
+    public string Date { get; set; } = null!;
     public int Flights { get; set; }
 }
